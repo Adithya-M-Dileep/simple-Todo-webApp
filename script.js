079e44a2c62a6ada0931fd8d3bf319c2cac125de
Adithya M Dileep <73463333+Adithya-M-Dileep@users.noreply.github.com>
2022-06-04 11:59:15 +0530

    Drag and drop feature

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function addNewElement(){
      isChecked:false,
      itemContent:inputText
  });
- 
- }
+
  viewAll();
+ }
 }
 
 
@@ -29,7 +29,8 @@ function addNewElement(){
 function displayRow(item){
     const node=document.createElement("div");
     node.setAttribute("id",item._id);
-    node.setAttribute("class","itemcontainer itemcontainer-S");
+    node.setAttribute("draggable",true);
+    node.setAttribute("class","itemcontainer itemcontainer-S draggable");
     node.innerHTML='<div class="checkBox"><input  type="checkbox"'+(item.isChecked ? "checked":"")+'/></div><div class="contentBox"><span>'+item.itemContent.slice(0,25)+'</span></div><div class="removeButtonBox"><button class="removeButton">x</button></div>';
     itemSection.appendChild(node);
     node.lastChild.firstChild.addEventListener("click",removeElement);
@@ -37,6 +38,14 @@ function displayRow(item){
     if(item.isChecked){
         node.getElementsByClassName("contentBox")[0].firstChild.classList.add("checkboxChecked");
     }
+
+    node.addEventListener("dragstart",()=>{
+        node.classList.add("dragging")
+    })
+    node.addEventListener("dragend",()=>{
+        node.classList.remove("dragging")
+        reOrder();
+    })
 }
 function displayElements(){   
     for(var i=0;i<itemList.length;i++){
@@ -129,3 +138,47 @@ document.addEventListener("keyup", function(event) {
         addNewElement();
     }
 });
+
+//Dragging functionality
+itemSection.addEventListener("dragover", e=>{
+    e.preventDefault();
+    const afterElement = getDragAfterElement(e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      itemSection.appendChild(draggable)
+    } else {
+      itemSection.insertBefore(draggable, afterElement)
+    }
+})
+
+function getDragAfterElement(y) {
+    const draggableElements = [...itemSection.querySelectorAll('.draggable:not(.dragging)')]
+  
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
+
+  function reOrder(){
+      let elements = [...itemSection.querySelectorAll('.draggable')];
+      itemList=[];
+      elements.forEach(x=>{
+          const elementId=x.id;
+          const elementIsChecked=x.getElementsByTagName("input")[0].checked;
+          const elementContent=x.getElementsByTagName("span")[0].innerHTML;
+          
+          itemList.push({
+            _id:elementId,
+            isChecked:elementIsChecked,
+            itemContent:elementContent
+        });
+       
+      })
+      viewAll();
+  }
